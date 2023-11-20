f9149e5d183329732eaf781da4401431873d07ab
Jerald Lashy Jeffery <jeraldjeffreylashie@gmail.com>
2023-11-20 08:19:01 +0200

    Ehh

---
+++ src/app/newDestination/page.jsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/footer";
+import Header from "@/components/generic-header";
+import React from "react";
+
+export default function NewDestinationPage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
