0e2d1a850fe6bd5ae3ef49bbda12261ca565d5e8
Jerald Lashy Jeffery <jeraldjeffreylashie@gmail.com>
2023-11-21 03:01:53 +0200

    hey

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -23,7 +23,7 @@ export default function LandingPage() {
           <Image
             src={"https://images.unsplash.com/photo-1498578726655-4a7188d5b17c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
             alt=""
-            layout="fill"
+            fill
             objectFit="cover"
             className="absolute inset-0 w-full object-cover z-0 filter brightness-75 opacity-25"
           />
